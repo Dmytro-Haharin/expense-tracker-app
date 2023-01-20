6636f4bbd5ffd398e0917951085c1fd799078c4a
DmTro <dmytrohaharin@gmail.com>
2023-01-20 17:40:23 +0300

    lesson1

---
+++ src/components/TaskForm.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <input v-model="newTask" type="text">
+        <button v-on:click="add">add</button>
+    </div>
+</template>
+
+<script>
+export default{
+    name:'TaskForm',
+    data(){
+        return{
+            newTask:''
+        }
+    },
+    methods:{
+        add(){
+            this.$emit('add', this.newTask)
+            this.newTask = ''
+        }
+    }
+}
+</script>
+
+<style leng="sass">
+</style>
